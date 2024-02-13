ac9989e3955209d196618494bb191b19da8a2841
Shaik2002 <83602965+Shaik2002@users.noreply.github.com>
2024-02-13 23:38:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleButtons = document.querySelectorAll(".toggle-btn");
+
+  toggleButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      // Remove 'selected' class from all plans
+      document.querySelectorAll(".plan").forEach(function(plan) {
+        plan.classList.remove("selected");
+      });
+
+      // Add 'selected' class to the clicked plan
+      button.parentNode.classList.add("selected");
+    });
+  });
+});
+
+
